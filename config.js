751c75e56b5d1db97e7d9dff9309d15e25236527
oranutph <x4577321@gmail.com>
2025-03-08 13:22:19 +0000

    แก้ไข

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    aprUrl: "https://script.google.com/macros/s/AKfycbxVUinvWzsp178oiD7ldBJFlc3J-Bke1377PsJ9uuBIbrKgI-TlxuO0xtzmqSV3af0b/exec"
+    aprUrl: "https://script.google.com/macros/s/AKfycbylrhjCzOpT2J2Zp4UeAVeCT2cw4ZHa86GpfxNrr4zUF35NCyVWi0rQY9QmrPd9XX9v/exec"
 }
